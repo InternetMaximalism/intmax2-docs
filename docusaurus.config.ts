1351056f175adb405ff4b0097a35c72124fcd231
signature18632 <intmax18@gmail.com>
2025-08-08 06:10:49 +0700

    added external link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,35 +2,24 @@ import type * as Preset from "@docusaurus/preset-classic";
 import type { Config } from "@docusaurus/types";
 import { themes as prismThemes } from "prism-react-renderer";
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
 const config: Config = {
   title: "INTMAX Network",
   tagline: "Efficient, Private, and Scalable Digital Asset Transactions",
   favicon: "img/favicon.ico",
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    v4: true,
   },
 
-  // Set the production url of your site here
   url: "https://docs.intmax.io",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "InternetMaximalism", // Usually your GitHub org/user name.
-  projectName: "intmax-docs-guide ", // Usually your repo name.
+  organizationName: "InternetMaximalism",
+  projectName: "intmax-docs ",
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
@@ -57,12 +46,11 @@ const config: Config = {
         id: "developers-hub",
         path: "developers-hub",
         routeBasePath: "developers-hub",
-        sidebarPath: require.resolve("./sidebars.ts"),
+        sidebarPath: require.resolve("./sidebars-developers-hub.ts"),
       },
     ],
   ],
 
-  // Enable the Docusaurus themes
   themes: [
     "@docusaurus/theme-mermaid",
     [
@@ -81,7 +69,6 @@ const config: Config = {
   },
 
   themeConfig: {
-    // Replace with your project's social card
     image: "img/social-card.jpg",
     navbar: {
       title: "",
